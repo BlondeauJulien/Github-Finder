ba66d0afb10dcb43a532a3b2e00552b7dad6f368
Julien Blondeau <julienblondeau@live.fr>
2019-10-13 23:49:45 +0200

    Create React Routes for home, About and single User

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './Components/Layout/Navbar';
 import Users from './Components/Users/Users';
+import User from './Components/Users/User';
 import Search from './Components/Users/Search';
 import Alert from './Components/Layout/Alert';
+import About from './Components/pages/About'
 import './App.css';
 import Axios from 'axios';
 
 class App extends Component {
 	state = {
-		users: [],
+    users: [],
+    user: {},
 		loading: false,
 		alert: null
 	};
@@ -30,7 +34,19 @@ class App extends Component {
 		);
 
 		this.setState({ users: res.data.items, loading: false });
-	};
+  };
+  
+  // Get single user
+  getUser = async (username) => {
+		this.setState({ loading: true });
+
+		const res = await Axios.get(
+			`https://api.github.com/users/${username}?&client_id=${process.env
+				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		this.setState({ user: res.data, loading: false });
+  }
 
 	clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -48,21 +64,40 @@ class App extends Component {
 	};
 
 	render() {
-		const { users, loading, alert } = this.state; // without this we would need to use this.props.users ...
+		const { users, user, loading, alert } = this.state; // without this we would need to use this.props.users ...
 		return (
-			<div className="App">
-				<Navbar />
-				<div className="container">
-					<Alert alert={alert} />
-					<Search
-						searchUsers={this.searchUsers}
-						clearUsers={this.clearUsers}
-						showClear={users.length > 0 ? true : false}
-						setAlert={this.setAlert}
-					/>
-					<Users loading={loading} users={users} />
+			<Router>
+				<div className="App">
+					<Navbar />
+					<div className="container">
+						<Alert alert={alert} />
+						<Switch>
+							<Route
+								exact path="/"
+								render={(props) => (
+									<Fragment>
+										<Search
+											searchUsers={this.searchUsers}
+											clearUsers={this.clearUsers}
+											showClear={users.length > 0 ? true : false}
+											setAlert={this.setAlert}
+										/>
+										<Users loading={loading} users={users} />
+									</Fragment>
+								)}
+							/>
+              <Route exact path="/about" component={About} />
+              <Route exact path="/user/:login" render={ props => (
+                <User { ...props } 
+                getUser={this.getUser} 
+                user={user}
+                loading={loading}
+                />
+              )}/>
+						</Switch>
+					</div>
 				</div>
-			</div>
+			</Router>
 		);
 	}
 }
--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 
 const Navbar = ({ icon, title}) => {
@@ -9,6 +10,16 @@ const Navbar = ({ icon, title}) => {
             <h1>
                 <i className={icon} /> {title}
             </h1>
+            <ul>
+                <li>
+{/*                     <a href="/">Home</a>  Using a tag could work to change pages but here for exmple if we have an active search and pass to home back to search it clear our state*/}
+                <Link to="/">Home</Link>
+                </li> 
+                <li>
+{/*                     <a href="/about">About</a> */}
+                <Link to="/about">About</Link>
+                </li> 
+            </ul>
         </nav>
     )
 
