c6c19ce523287229256607e8873b192f5c9fa94e
Julien Blondeau <julienblondeau@live.fr>
2019-10-14 18:35:59 +0200

    Turn React Class component - Search - into a Hook component

--- src/Components/Users/Search.js
+++ src/Components/Users/Search.js
@@ -1,48 +1,43 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-export class Search extends Component {
-    state = {
-        text: ''
-    }
+const Search = ( { searchUsers, setAlert, showClear, clearUsers } ) => {
+    const [text, setText] = useState('');
 
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        setAlert: PropTypes.func.isRequired,
-    }
-
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
 
-        if(this.state.text.trim() === '') {
-            this.props.setAlert('Please enter something', 'light');
+        if(text.trim() === '') {
+            setAlert('Please enter something', 'light');
         } else {
-            this.props.searchUsers(this.state.text)
-            this.setState({ text: ''})
+            searchUsers(text)
+            setText(text)
         }
     } //without arrow function this is undefined so we need to explictely bind it in onSubmit onSubmit={this.onSubmit.bind(this)}
 
-    onChange = (e) => this.setState( { [e.target.name]: e.target.value });
+    const onChange = (e) => setText(e.target.value);
     
+    return (
+        <div>
+            <form onSubmit={onSubmit} className="form">
+                <input type="text" name="text" placeholder="Search Users ..." 
+                value={text}
+                onChange={onChange}
+                />
+                <input type="submit" value="Search" className="btn btn-dark btn-block" />
+            </form>
+            {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
+            
+        </div>
+    )
+}
 
-    render() {
-        const { showClear, clearUsers } = this.props;
-        return (
-            <div>
-                <form onSubmit={this.onSubmit} className="form">
-                    <input type="text" name="text" placeholder="Search Users ..." 
-                    value={this.state.text}
-                    onChange={this.onChange}
-                    />
-                    <input type="submit" value="Search" className="btn btn-dark btn-block" />
-                </form>
-                {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
-                
-            </div>
-        )
-    }
+
+Search.propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
 }
 
 export default Search
--- src/Components/repos/RepoItem.js
+++ src/Components/repos/RepoItem.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import { blockStatement } from '@babel/types';
 
 const RepoItem = ( { repo } ) => {
     return (
